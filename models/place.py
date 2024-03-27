ad237ea7ed602700343ffebe94b58bedac3f82ac
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-03-27 18:18:52 +0400

    adding amenty model

--- models/place.py
+++ models/place.py
@@ -11,8 +11,10 @@ from models import storage
 place_association = Table(
     'place_amenity',
     Base.metadata,
-    Column('place_id', String(60), ForeignKey("places.id"), primary_key=True),
-    Column('amenity_id', String(60), ForeignKey("amenities.id"), primary_key=True)
+    Column('place_id', String(60), ForeignKey("places.id"),
+           primary_key=True),
+    Column('amenity_id', String(60), ForeignKey("amenities.id"),
+           primary_key=True)
     )
 
 
