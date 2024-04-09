db3abd5d37a2c5a292c808c89d18b31b13e5763e
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-04-09 16:18:08 +0400

    adding default value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Display c {text}.
+    /python/<text>: Display python {text}.
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def what_is_c(text):
+    """Display C {text}"""
+    return (f"C {escape(text.replace('_', ' '))}")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def what_is_python(text="is cool"):
+    """Display python {text}"""
+    return (f"Python {escape(text.replace('_', ' '))}")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
