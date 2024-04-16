39dac046c52d33fa8521a7d256878c355c7877ee
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-04-16 10:50:39 +0400

    number odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Display c {text}.
+    /python/<text>: Display python {text}.
+    /number/<n>: Display number.
+    /number_odd_or_even/<n>: Display number odd or even.
+"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask("__name__",  template_folder="web_flask/templates")
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def what_is_c(text):
+    """Display C {text}"""
+    return (f"C {escape(text.replace('_', ' '))}")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def what_is_python(text="is cool"):
+    """Display python {text}"""
+    return (f"Python {escape(text.replace('_', ' '))}")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def what_is_number(n: int):
+    """Display number"""
+    return (f"{escape(n)} is number")
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def show_number(n: int):
+    """Display number in the html page"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_even(n: int):
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
